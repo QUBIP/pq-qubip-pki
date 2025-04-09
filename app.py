36e9519374a2e91b16b7e6f87072abc19e41cb08
Grazia D'Onghia <grazia.donghia@polito.it>
2025-04-09 12:08:37 +0200

    removed certs

--- app.py
+++ app.py
@@ -239,4 +239,4 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='130.192.1.31', ssl_context=('server-chain.pem', 'server.key'))
